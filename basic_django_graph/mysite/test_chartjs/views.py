c1be1e4af9dc1d7923255ee5b5d57fe8e2605ac3
yoshiyukiminami <minami@agsoil.jp>
2022-01-23 15:27:52 +0900

    filterの使用と年月日表示の修正

--- basic_django_graph/mysite/test_chartjs/views.py
+++ basic_django_graph/mysite/test_chartjs/views.py
@@ -5,7 +5,7 @@ from .models import Temperature
 
 def index(request):
     """いわばhtmlのページ単位の構成物です"""
-    context = {'latest_list': Temperature.objects.order_by('ymd')}
+    context = {'latest_list': Temperature.objects.filter(ymd__range=["2021-01-10 00:00:00", "2021-01-21 00:00:00"]).order_by('ymd')}
     print(context)
     # htmlとして返却します
     return render(request, 'test_chartjs/index.html', context)
