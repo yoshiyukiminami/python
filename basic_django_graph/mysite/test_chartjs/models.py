da648214dfc36e811fb1af2c1aee136284330924
yoshiyukiminami <minami@agsoil.jp>
2022-01-23 14:12:25 +0900

    行間スペースの修正

--- basic_django_graph/mysite/test_chartjs/models.py
+++ basic_django_graph/mysite/test_chartjs/models.py
@@ -4,13 +4,12 @@ from django.db import models
 # クラス名がテーブル名です
 class DjangoTestTable(models.Model):
     # ここに定義したものがフィールド項目です
-    month_code = models.CharField(default='XXX', max_length=3)   # Jun Feb など
+    month_code = models.CharField(default='XXX', max_length=3)  # Jun Feb など
     sales = models.IntegerField(default=0)
     pub_date = models.DateTimeField('date published')
 
+
 class Temperature(models.Model):
-
-
     ymd = models.DateTimeField()
     kiatsu_riku = models.FloatField(default=0, null=True)
     kiatsu_umi = models.FloatField(default=0, null=True)
