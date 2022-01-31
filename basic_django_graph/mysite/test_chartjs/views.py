d76844e9d9adacee6e05d9c863c9e87741f3b203
yoshiyukiminami <minami@agsoil.jp>
2022-01-31 09:37:25 +0900

    #7 ""このmodels.pyは「graph」の「test_scraping.py」にも影響ありますよね？そこで、現在作成中の「past_data_scraping」用のテーブルセットを「Pasttemperature」として追加しました。"" →上記の変更をかけた結果、views.pyの「Temperature」が自動的に「Pasttemperature」に変更されていたため、再度「temperature」に戻したのですが、objectsエラーが出ています。また、これと同じコードを「Pasttemperature」でも作っておかないといけない？

--- basic_django_graph/mysite/test_chartjs/views.py
+++ basic_django_graph/mysite/test_chartjs/views.py
@@ -9,3 +9,15 @@ def index(request):
     print(context)
     # htmlとして返却します
     return render(request, 'test_chartjs/index.html', context)
+
+#一番最初のコマンド（filterを入れる前）
+# """子供のurls.pyがこの処理を呼び出します"""
+# from django.shortcuts import render
+# from .models import Temperature
+#
+#
+# def index(request):
+#     """いわばhtmlのページ単位の構成物です"""
+#     context = {'latest_list': Temperature.objects.order_by('ymd')}
+#     # htmlとして返却します
+#     return render(request, 'test_chartjs/index.html', context)
