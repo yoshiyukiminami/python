6f748f639718a24dcf070db93e6159bf67e2defb
yoshiyukiminami <minami@agsoil.jp>
2022-02-20 13:06:48 +0900

    #16 入力フォーム作成途中3 入力フォームの設計を以下の修正 ①設定したい期間の開始年・月・日を入力する ②設定したい期間の終了年・月・日を入力する ③比較したい過去データの期間（何年間）を入力する
    各関数内で未定義の変数（例：check_input_kikan_start_day()の中のkikan_start_year）にはダミーを代入して、各関数をテスト済み

--- graph/kikan_input.py
+++ graph/kikan_input.py
@@ -1,44 +1,41 @@
+import calendar
 import datetime
-from calendar import isleap
 
 # 入力時の年と月を取得
 this_year = datetime.date.today().year
 this_month = datetime.date.today().month
 
 
-def check_input_start_year():
+def check_input_kikan_start_year():
     """
-    ①過去データ開始年度入力
+    ①設定したい期間の開始年を入力
     :rtype: object
     """
-    kako_start_year = None
+    kikan_start_year = None
     while True:
         try:
-            kako_start_year = int(input("比較する過去データの開始年度を入力してください: "))
+            kikan_start_year = int(input("設定したい期間の開始年を入力してください: "))
         except ValueError:
             print("入力値が無効です。再入力してください。")
             continue
         else:
-            if kako_start_year < 2002:
-                print("2002年以降の年を入力してください")
-                continue
-            elif kako_start_year > this_year:
-                print("本年度もしくはそれ以前の年を入力してください")
+            if not (2002 <= kikan_start_year <= this_year):
+                print("2002年以降で今年までの数値を入力してください")
                 continue
             else:
                 break
-    return kako_start_year
+    return kikan_start_year
 
 
-def check_input_start_month():
+def check_input_kikan_start_month():
     """
-    ②比較する期間の開始月入力
+    ②設定したい期間の開始月を入力
     :rtype: object
     """
     kikan_start_month = None
     while True:
         try:
-            kikan_start_month = int(input("比較する期間の開始月を入力してください: "))
+            kikan_start_month = int(input("設定したい期間の開始月を入力してください: "))
         except ValueError:
             print("入力値が無効です。再入力してください。")
             continue
@@ -51,70 +48,119 @@ def check_input_start_month():
     return kikan_start_month
 
 
-def check_input_start_day(year, kikan_start_month):
+def check_input_kikan_start_day():
     """
-    ②比較する期間の開始日入力
+    ③設定したい期間の開始日を入力
     :rtype: object
     """
-
     kikan_start_day = None
+    kikan_start_year = 2021
+    kikan_start_month = 1
+    month_count = calendar.monthrange(kikan_start_year, kikan_start_month)[1]
+    print(month_count)
     while True:
         try:
-            kikan_start_day = int(input("比較する期間の開始日を入力してください: "))
+            kikan_start_day = int(input("設定したい期間の開始日を入力してください: "))
         except ValueError:
             print("入力値が無効です。再入力してください。")
             continue
         else:
-            if not (1 <= kikan_start_day <= ):
+            if not (1 <= kikan_start_day <= month_count):
+                print("設定したい開始月の日数範囲にある数値を入力してください")
+                continue
+            else:
+                break
+    return kikan_start_day
+
+
+def check_input_kikan_stop_year():
+    """
+    ①設定したい期間の終了年を入力
+    :rtype: object
+    """
+    kikan_stop_year = None
+    kikan_start_year = 2021
+    while True:
+        try:
+            kikan_stop_year = int(input("設定したい期間の終了年を入力してください: "))
+        except ValueError:
+            print("入力値が無効です。再入力してください。")
+            continue
+        else:
+            if not (kikan_start_year <= kikan_stop_year <= this_year):
+                print("開始年以降で今年までの数値を入力してください")
+                continue
+            else:
+                break
+    return kikan_stop_year
+
+
+def check_input_kikan_stop_month():
+    """
+    ②設定したい期間の終了月を入力
+    :rtype: object
+    """
+    kikan_stop_month = None
+    while True:
+        try:
+            kikan_stop_month = int(input("設定したい期間の終了月を入力してください: "))
+        except ValueError:
+            print("入力値が無効です。再入力してください。")
+            continue
+        else:
+            if not (1 <= kikan_stop_month <= 12):
                 print("1以上12以下の数値を入力してください")
                 continue
             else:
                 break
-    return kikan_start_month
-# # ③比較する期間の開始日入力
-# while True:
-#     try:
-#         kikan_start_day = int(input(": "))
-#     except ValueError:
-#         print("入力値が無効です。再入力してください。")
-#         continue
-#     else:
-#         break
-# if kikan_start_day < 1:
-#     print("1以上の数値を入力してください")
-# elif kikan_start_day > end_day:
-#     print(str(end_day) and "以下の数値を入力してください")
-# else:
-#     print(kikan_start_day)
-#
-#
-# # ④比較する期間の終了月入力
-# while True:
-#     try:
-#         kikan_stop_month = int(input("比較する期間の終了月を入力してください: "))
-#     except ValueError:
-#         print("入力値が無効です。再入力してください。")
-#         continue
-#     else:
-#         break
-# if 1 > kikan_stop_month > 12:
-#     print("1以上12以下の数値を入力してください")
-# else:
-#     print(kikan_stop_month)
-#
-#
-# # ⑤比較する期間の終了日入力
-# while True:
-#     try:
-#         kikan_stop_day = int(input("比較する期間の終了日を入力してください: "))
-#     except ValueError:
-#         print("入力値が無効です。再入力してください。")
-#         continue
-#     else:
-#         break
-# if kikan_stop_day < 1:
-#     print("1以上の数値を入力してください")
-# elif kikan_stop_day > end_day:
-#     print(str(end_day) and "以下の数値を入力してください")
-# else:
-#     print(kikan_start_day)
+    return kikan_stop_month
+
+
+def check_input_kikan_stop_day():
+    """
+    ③設定したい期間の終了日を入力
+    :rtype: object
+    """
+    kikan_stop_day = None
+    kikan_stop_year = 2022
+    kikan_stop_month = 2
+    month_count = calendar.monthrange(kikan_stop_year, kikan_stop_month)[1]
+    print(month_count)
+    while True:
+        try:
+            kikan_stop_day = int(input("設定したい期間の終了日を入力してください: "))
+        except ValueError:
+            print("入力値が無効です。再入力してください。")
+            continue
+        else:
+            if not (1 <= kikan_stop_day <= month_count):
+                print("設定したい開始月の日数範囲にある数値を入力してください")
+                continue
+            else:
+                break
+    return kikan_stop_day
+
+
+def check_input_kako_compare_kikan():
+    """
+    ⑦比較する過去データの期間を選択
+    :rtype: object
+    """
+    kako_data_kikan = None
+    kikan_start_year = 2010
+    while True:
+        try:
+            kako_data_kikan = int(input("比較したい過去データの期間（1～20までの整数）を入力してください: "))
+        except ValueError:
+            print("入力値が無効です。再入力してください。")
+            continue
+        else:
+            if not (1 <= kako_data_kikan <= 20):
+                print("1～20までの整数を入力してください")
+                continue
+            elif (kikan_start_year - 2002) < kako_data_kikan:
+                print("過去データがないため、短い期間を再入力してください")
+                continue
+            else:
+                break
+    return kako_data_kikan
--- graph/test_kikan_input.py
+++ graph/test_kikan_input.py
@@ -1,15 +1,40 @@
 from unittest import TestCase
 
-from graph.kikan_input import check_input_start_year, check_input_start_month
+from graph.kikan_input import check_input_kikan_start_day, check_input_kikan_start_month, \
+    check_input_kikan_start_year, check_input_kako_compare_kikan, check_input_kikan_stop_year, \
+    check_input_kikan_stop_month, check_input_kikan_stop_day
 
 
-class Test1(TestCase):
-    def test_check_input_start_year(self):
-        self.assertEqual(2002, check_input_start_year())
+class Test(TestCase):
+    def test_check_input_kikan_start_year(self):
+        self.assertEqual(2021, check_input_kikan_start_year())
 
 
 class Test2(TestCase):
-    def test_check_input_start_month(self):
-        self.assertEqual(12, check_input_start_month())
+    def test_check_input_kikan_start_month(self):
+        self.assertEqual(1, check_input_kikan_start_month())
 
 
+class Test3(TestCase):
+    def test_check_input_kikan_start_day(self):
+        self.assertEqual(10, check_input_kikan_start_day())
+
+
+class Test4(TestCase):
+    def test_check_input_kikan_stop_year(self):
+        self.assertEqual(2022, check_input_kikan_stop_year())
+
+
+class Test5(TestCase):
+    def test_check_input_kikan_stop_month(self):
+        self.assertEqual(2, check_input_kikan_stop_month())
+
+
+class Test7(TestCase):
+    def test_check_input_kako_compare_kikan(self):
+        self.assertEqual(10, check_input_kako_compare_kikan())
+
+
+class Test6(TestCase):
+    def test_check_input_kikan_stop_day(self):
+        self.assertEqual(28, check_input_kikan_stop_day())
