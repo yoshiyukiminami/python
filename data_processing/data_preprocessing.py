3cca126de25c2a99549292ce41bab7a8a297b459
yoshiyukiminami <minami@agsoil.jp>
2022-05-01 13:38:05 +0900

    #25 Step2-1まで作成、検証済み

--- data_processing/data_preprocessing.py
+++ data_processing/data_preprocessing.py
@@ -12,7 +12,7 @@
 # 【action_4】Step_4まで通過したデータから作土層管理サービス（仮）に必要な数値を計算し、table3（作土層管理サービス）に格納
 
 import pandas as pd
-import datetime
+# import datetime
 
 # 【下準備1】表示の制限・・jupyterlabのみ有効
 pd.set_option('display.max_rows', 10)
@@ -36,7 +36,12 @@ else:
 # 【Step_1】農場名および圃場名の空欄（データなし）検知＆空欄行表示・・エラー01、02
 df_nojyomei = df['農場名']
 df_hojyomei = df['圃場名']
-if not df_nojyomei.isnull().all():
+df_nojyomei_list = list(set(df_nojyomei))
+df_hojyomei_list = list(set(df_hojyomei))
+print(df_nojyomei_list, df_hojyomei_list)
+
+isvalid = True
+if not df_nojyomei.notnull().all():
     print("エラー01：農場名に空欄があります")
     print("空欄のある行は以下の通り・・")
     print(df.query('農場名 != 農場名'))
@@ -44,10 +49,10 @@ if not df_nojyomei.isnull().all():
     #     if df_nojyomei[n] == float('nan'):
     #         print("エラー01：農場名に空欄があります")
     #         print("農場名の空欄は" + str(n + 1) + "行目")
-    exit()
+    isvalid = False
 else:
     print("農場名に空欄はありません")
-    if not df_hojyomei.isnull.all():
+    if not df_hojyomei.notnull().all():
         print("エラー02：圃場名に空欄があります")
         print("空欄のある行は以下の通り・・")
         print(df.query('圃場名 != 圃場名'))
@@ -55,7 +60,7 @@ else:
         #     if df_hojyomei[n] == float('nan'):
         #         print("エラー02：圃場名に空欄があります")
         #         print("圃場名の空欄は" + str(n + 1) + "行目")
-        exit()
+        isvalid = False
     else:
         print("圃場名に空欄はありません")
         print("農場名と圃場名の組合せでデータの並び替えを行いました")
@@ -63,117 +68,62 @@ else:
 # 【Step_2-1】紐付け情報エラーA・・圃場内位置、圃場内位置2の記入漏れまたは入力条件エラー（A～E以外、1~5以外、文字属性）・・エラー03～06
 df_point1 = df['圃場内位置']
 df_point2 = df['圃場内位置2']
+print(df_point1, df_point2)
 
 #  測定位置の範囲をリスト化
 point1_list = ['A', 'B', 'C', 'D', 'E']
 point2_list = [1, 2, 3, 4, 5]
 
-#  圃場内位置および圃場内位置2の空欄（データなし）を検知＆空欄行表示・・エラー03、04
-if not df_point1.isnull().all():
-    print("エラー03：圃場内位置に空欄があります")
-    print("空欄のある行は以下の通り・・")
-    print(df.query('圃場内位置 != 圃場内位置'))
-    # for n in range(len(df)):
-    #     if df_point1[n] == float('nan'):
-    #         print("エラー03：圃場内位置に空欄があります")
-    #         print("圃場内位置の空欄は" + str(n + 1) + "行目")
-    exit()
-else:
-    print("圃場内位置に空欄はありません")
-    if not df_point2.isnull.all():
-        print("エラー04：圃場内位置2に空欄があります")
+if isvalid:
+    #  圃場内位置および圃場内位置2の空欄（データなし）を検知＆空欄行表示・・エラー03、04
+    if not df_point1.notnull().all():
+        print("エラー03：圃場内位置に空欄があります")
         print("空欄のある行は以下の通り・・")
-        print(df.query('圃場内位置2 != 圃場内位置2'))
+        print(df.query('圃場内位置 != 圃場内位置'))
         # for n in range(len(df)):
-        #     if df_point2[n] == float('nan'):
-        #         print("エラー04：圃場内位置2に空欄があります")
-        #         print("圃場内位置2の空欄は" + str(n + 1) + "行目")
-        exit()
+        #     if df_point1[n] == float('nan'):
+        #         print("エラー03：圃場内位置に空欄があります")
+        #         print("圃場内位置の空欄は" + str(n + 1) + "行目")
+        isvalid = False
     else:
-        print("圃場内位置2に空欄はありません")
+        print("圃場内位置に空欄はありません")
+        if not df_point2.notnull().all():
+            print("エラー04：圃場内位置2に空欄があります")
+            print("空欄のある行は以下の通り・・")
+            print(df.query('圃場内位置2 != 圃場内位置2'))
+            # for n in range(len(df)):
+            #     if df_point2[n] == float('nan'):
+            #         print("エラー04：圃場内位置2に空欄があります")
+            #         print("圃場内位置2の空欄は" + str(n + 1) + "行目")
+            isvalid = False
+        else:
+            print("圃場内位置2に空欄はありません")
 
-# 圃場内位置の入力条件エラー：リスト（A～E）以外の文字・数値で入力検知＆エラー行表示
-for i in df_point1:
-    if i not in point1_list:
-        print("エラー05：圃場内位置に無効な文字が入力されています")
-        print("無効な文字・数字が入力されている行は以下の通りです")
-        print(df.query('not 圃場内位置 in point1_list'))
-        exit()
-    else:
-        print("圃場内位置に無効な文字・数字はありません")
-        # 圃場内位置2の入力条件エラー検知：リスト（1～5）以外の数値や文字で入力＆エラー行表示
-        for j in df_point2:
-            if j not in point2_list:
-                print("エラー06：圃場内位置2に無効な数値が入力されています")
+    if isvalid:
+        # 圃場内位置の入力条件エラー：リスト（A～E）以外の文字・数値で入力検知＆エラー行表示
+        for i in df_point1:
+            if i not in point1_list:
+                print("エラー05：圃場内位置に無効な文字が入力されています")
                 print("無効な文字・数字が入力されている行は以下の通りです")
-                print(df.query('not 圃場内位置2 in point2_list'))
-                exit()
-            else:
+                print(df.query("not 圃場内位置 in ['A', 'B', 'C', 'D', 'E']"))
+                isvalid = False
+        if isvalid:
+            print("圃場内位置に無効な文字・数字はありません")
+
+        if isvalid:
+            # 圃場内位置2の入力条件エラー検知：リスト（1～5）以外の数値や文字で入力＆エラー行表示
+            for j in df_point2:
+                if j not in point2_list:
+                    print("エラー06：圃場内位置2に無効な数値が入力されています")
+                    print("無効な文字・数字が入力されている行は以下の通りです")
+                    print(df.query("not 圃場内位置2 in [1, 2, 3, 4, 5]"))
+                    isvalid = False
+            if isvalid:
                 print("圃場内位置2に無効な文字・数字はありません")
 
-# ##ここから未処理##
-
-# 【Step_2-2】紐付け情報エラーB・・品目、測定日、時期の記入漏れまたは入力条件エラー（1項目に複数入力、文字属性）・・エラー
-
-# Step-2-2：【下処理1】農場名での分類
-nojyomei_list = df['農場名'].tolist()
-nojyomei_list = list(set(nojyomei_list))
-nojyomei_list_count = len(nojyomei_list)
-
-# 農場名が空欄または複数ある場合にエラー表示
-if nojyomei_list_count > 1:
-    print('エラー03：農場名が空欄または複数あります')
-else:
-    # print(nojyomei_list)
-    # Step-2-2：紐付け情報エラーB・・品目・測定日・時期の記入漏れまたは入力条件エラー検知（測定日：データ型、時期：登録外）
-    for k in range(nojyomei_list_count):
-        nojyomei = df['農場名'][k]
-        df1 = df[df['農場名'] == nojyomei]
-        hojyomei_list = df1['圃場名'].tolist()
-        hojyomei_list = list(set(hojyomei_list))
-        hojyomei_list_count = len(hojyomei_list)
-        for m in range(len(hojyomei_list)):
-            hojyomei = hojyomei_list[m]
-            df2 = df1[df1['圃場名'] == hojyomei]
-
-            # 品目・時期の入力条件エラー検知（空欄、複数）+エラーのある圃場名を表示
-            item_list = df2['品目'].tolist()
-            item_list_count = len(set(item_list))
-            if not item_list_count == 1:
-                print("エラー04：品目が空欄または複数あります")
-                print('圃場名：' + hojyomei)
-                break
-            else:
-                jiki_list = df2['時期'].tolist()
-                jiki_list_count = len(set(jiki_list))
-                if not jiki_list_count == 1:
-                    print("エラー05：時期が空欄または複数あります")
-                    print('圃場名：' + hojyomei)
-                    break
-                else:
-                    # 測定日の入力条件エラー検知（空欄、複数、表示形式）+エラーのある圃場名を表示
-                    sokuteibi_list = df2['測定日'].tolist()
-                    sokuteibi_list = list(set(sokuteibi_list))
-                    sokuteibi_list_count = len(sokuteibi_list)
-                    print(sokuteibi_list, sokuteibi_list_count)
-                    if not sokuteibi_list_count == 1:
-                        print("エラー06：測定日が空欄または複数あります")
-                        print('圃場名：' + hojyomei)
-                        break
-                    else:
-                        try:
-                            sokuteibi = sokuteibi_list[m] + ' ' + '00:00:00'
-                            sokuteibi = datetime.datetime.strptime(sokuteibi,
-                                                                   '%Y.%m.%d %H:%M:%S')
-                            print(sokuteibi)
-                        except ValueError:
-                            print("エラー07：測定日の表示形式が間違っています")
-                            print('圃場名：' + hojyomei)
-                            break
 # ##ここまで未処理##
 
 # 【Step_5】並び替え・・農場名、圃場名が同じ組み合わせで昇順
 df = df.sort_values(by=['農場名', '圃場名', '圃場内位置', '圃場内位置2'], ascending=True)
 print(df, type(df))
 # df.to_csv('df.csv', encoding='Shift-JIS', index=None)
-
