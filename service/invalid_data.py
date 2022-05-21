5244a198a0d64119831184d5f52ffb754f6267a2
yoshi <yoshitakaOkada0214@gmail.com>
2022-05-21 21:40:42 +0900

    すべて無効値だったときのエラーを追加

--- service/invalid_data.py
+++ service/invalid_data.py
@@ -72,6 +72,8 @@ def find_invalid_data(file_path: str, adjustment: bool):
         end_col_in_a_line = get_cursor_of_spike(line, under_threshold, offset, True)
         if not adjustment:
             line = line[first_col_in_a_line:end_col_in_a_line + 1]  # slice の end は "未満" なので注意する
+            if len(line) == 0:
+                records.append([str(i + 1) + f"行目のデータは無効なデータ値 {under_threshold} のみで構成されています。確認してください"])
             for col, cell in enumerate(line):
                 if cell == under_threshold:
                     records.append([str(i + 1) + f"行目のデータは無効なデータ値 {under_threshold} が含まれています"])
