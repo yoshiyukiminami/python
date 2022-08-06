5f4f825896f885fafd7c0d6b11c073ce2138a219
yoshiyuki minami <minami@agsoil.jp>
2022-08-06 13:28:29 +0900

    #36 エクセルファイルの読みこみまで     グラフ化（窒素関連）は一旦完成

--- soil_analysis/hiryo/soil_chemical_properties.py
+++ soil_analysis/hiryo/soil_chemical_properties.py
@@ -14,35 +14,46 @@ def graphset_N(df2):
     # グラフのタイトルを生成する
     graph_title = df2[['生産者名', '圃場名', '品目', '作型', '採土日']].values
     title = '窒素関連_' + '_'.join(graph_title)
+    hojyomei = df2['圃場名']
     # df2からグラフに必要な項目のみ分離
     df_N = df2.iloc[[5, 17, 18, 26, 27]]
     # データを換算するための基準値設定
     dataset_N = {'EC(mS/cm)': 0.2, 'NH4-N(mg/100g)': 1.5, 'NO3-N(mg/100g)': 3.5, '無機態窒素': 15, 'NH4/無機態窒素': 0.6}
+    # kijyunkoumoku = ['EC(mS/cm)', 'NH4-N(mg/100g)', 'NO3-N(mg/100g)', '無機態窒素', 'NH4/無機態窒素']
+    # kijyunti = [0.2, 1.5, 3.5, 15, 0.6]
+    # dataset_N2 = pd.DataFrame(kijyunti, index=kijyunkoumoku)
+    # print(dataset_N2)
+
     # df_Nの数値を基準値で除算してパーセントに変換
-    for k in dataset_N:
-        x = df_N[k] / dataset_N[k]
-        df_N[k] = x
-    print(df_N)
+    for k, j in enumerate(dataset_N):
+        x = df_N[j] / dataset_N[j]
+        df_N[j] = x
+    df_N2 = pd.DataFrame(df_N)
+    # print(df_N)
     # 窒素に関連する土壌化学性項目グラフを生成・保存
     fig = go.Figure()
-    for n, m in enumerate(df_N):
+    for n, m in df_N2.iterrows():
         # 計算値が100％以上の時にBAR色を赤、100％未満はグレー
-        if m >= 1:
+        x = m.values
+        if x >= 1:
             color = 'red'
         else:
             color = 'grey'
-        print(color)
-        print(n, m)
-        y = df_N.iloc[n]
-        print(y, type(y))
+        y = [n]
+        s1 = df_N[n]
+        print(s1)
+        print('==')
+        s2 = dataset_N[n]
+        print(s2)
+        print('===')
         # 項目毎に計算値を系列追加
         fig.add_trace(go.Bar(y=y,
-                             x=np.array(m),
-                             name=df_N.iloc[n],
-                             width=0.1,
+                             x=x,
+                             name=hojyomei,
+                             width=0.7,
                              marker_color=color,
                              showlegend=False,
-                             # hovertext='飽和度:%{x}, 測定項目:%{k}',
+                             # hovertext='測定値:%{s1}, 基準値:%{s2}',
                              orientation='h')
                       )
     fig.update_layout(title=dict(text=title,
@@ -52,7 +63,6 @@ def graphset_N(df2):
                                  y=0.85,
                                  xanchor='left'
                                  ),
-                      # yaxis=dict(title='測定項目', range=(0.5, koumoku+0.5)),
                       yaxis=dict(title='測定項目'),
                       xaxis=dict(title='飽和度（基準値100％）', range=(0, 2), tickformat='%'),
                       width=500,
@@ -70,11 +80,16 @@ def graphset_N(df2):
     # # fig.write_image(fig_name2)
     fig.show()
 
-def graphset_enki(df2):
+
+def graphset_P(df2):
     print(df2)
 
 
-def graphset_soilpotential(df2):
+def graphset_Enki(df2):
+    print(df2)
+
+
+def graphset_Soilpotential(df2):
     print(df2)
 
 
@@ -102,6 +117,7 @@ if __name__ == '__main__':
                 print("データは正常です")
                 df2 = df.loc[i]
                 graphset_N(df2)
-                # graphset_enki(df2)
-                # graphset_soilpotential(df2)
+                # graphset_P(df2)
+                # graphset_Enki(df2)
+                # graphset_Soilpotential(df2)
 
