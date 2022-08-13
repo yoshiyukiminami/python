6aefeac4bb2e428295eb0315e2e042043f8007ba
yoshi <yoshitakaOkada0214@gmail.com>
2022-08-13 21:20:41 +0900

    db setting

--- soil_analysis/config/settings.py
+++ soil_analysis/config/settings.py
@@ -76,11 +76,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '127.0.0.1',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'soil_db',
+        'USER': 'python',
+        'PASSWORD': 'python123',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
