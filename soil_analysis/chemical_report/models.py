0e64758eedd732ddb953595305bef08db861cb4b
yoshi <yoshitakaOkada0214@gmail.com>
2022-08-15 19:48:42 +0900

    db定義調整

--- soil_analysis/chemical_report/models.py
+++ soil_analysis/chemical_report/models.py
@@ -29,13 +29,13 @@ class Fields(models.Model):
     prefecture  都道府県    e.g. 茨城県
     location    住所       e.g. 結城郡八千代町
     latlon      緯度経度    e.g. 36.164677272061,139.86772928159
-    cultivation_method 栽培方法  e.g. 露地、ビニールハウス
+    class_clops 栽培方法  e.g. 露地、ビニールハウス
     """
     name = models.CharField(max_length=256)
     prefecture = models.CharField(max_length=256)
     location = models.CharField(max_length=256)
     latlon = models.CharField(null=True, max_length=256)
-    cultivation_method = models.CharField(max_length=256)
+    class_clops = models.CharField(max_length=256)
     remark = models.TextField(null=True)
     companies = models.ForeignKey('Companies', on_delete=models.CASCADE)
     clops = models.ForeignKey('Clops', on_delete=models.CASCADE)
@@ -106,39 +106,39 @@ class FieldAreaDetail(models.Model):
     """
     顧客が持つ圃場ひとつのエリアひとつを１レコードという単位で収録します
     圃場ひとつは９つのエリアに分かれる
-    ec                      電気伝導度         e.g. 1(mS/cm)
-    nh4n                    アンモニア性窒素    e.g. 1(mg/100g)
-    no3n                    硝酸態窒素         e.g. 1(mg/100g)
-    inorganic_nitrogen      無機態窒素 ??
-    nh4                     無機態窒素 ??
-    ph                      酸性アルカリ性度合い e.g. 0.6
-    cao                     酸化カルシウム
-    mgo                     マグネシウム
-    k2o                     カリ
-    base_saturation         塩基飽和度         e.g. 0.57
-    cao_per_mgo             CaO/MgO          e.g. 0.57
-    mgo_per_k2o             MgO/K2O          e.g. 0.57
-    phosphorus_absorption   リン吸収
-    p2o5                    五酸化リン
+    ec                      電気伝導率 e.g. 1(mS/cm)
+    nh4n                    アンモニア態窒素 e.g. 1(mg/100g)
+    no3n                    硝酸態窒素 e.g. 1(mg/100g)
+    nh4no3                  無機態窒素（NH4＋NO3）
+    nh4_per_nh4no3          アンモニア態窒素比
+    ph                      水素イオン濃度
+    cao                     交換性石灰
+    mgo                     交換性苦土
+    k2o                     交換性加里
+    base_saturation         塩基飽和度 e.g. 0.57
+    cao_per_mgo             CaO/MgO e.g. 0.57
+    mgo_per_k2o             MgO/K2O e.g. 0.57
+    phosphorus_absorption   リン酸吸収係数
+    p2o5                    可給態リン酸
     cec                     塩基置換容量
     humus                   腐植
     bulk_density            仮比重
     """
-    ec = models.IntegerField(null=True)
-    nh4n = models.IntegerField(null=True)
-    no3n = models.IntegerField(null=True)
-    inorganic_nitrogen = models.IntegerField(null=True)
-    nh4 = models.IntegerField(null=True)
+    ec = models.FloatField(null=True)
+    nh4n = models.FloatField(null=True)
+    no3n = models.FloatField(null=True)
+    nh4no3 = models.FloatField(null=True)
+    nh4_per_nh4no3 = models.FloatField(null=True)
     ph = models.FloatField(null=True)
-    cao = models.IntegerField(null=True)
-    mgo = models.IntegerField(null=True)
-    k2o = models.IntegerField(null=True)
+    cao = models.FloatField(null=True)
+    mgo = models.FloatField(null=True)
+    k2o = models.FloatField(null=True)
     base_saturation = models.FloatField(null=True)
     cao_per_mgo = models.FloatField(null=True)
     mgo_per_k2o = models.FloatField(null=True)
-    phosphorus_absorption = models.IntegerField(null=True)
-    p2o5 = models.IntegerField(null=True)
-    cec = models.IntegerField(null=True)
+    phosphorus_absorption = models.FloatField(null=True)
+    p2o5 = models.FloatField(null=True)
+    cec = models.FloatField(null=True)
     humus = models.FloatField(null=True)
     bulk_density = models.FloatField(null=True)
     remark = models.TextField(null=True)
--- soil_analysis/chemical_report/views.py
+++ soil_analysis/chemical_report/views.py
@@ -20,7 +20,7 @@ class SummaryReport(ListView):
         query = FieldAreaDetail.objects.get(id=1)
         x = ['EC(mS/cm)', 'NH4-N(mg/100g)', 'NO3-N(mg/100g)', '無機態窒素', 'NH4/無機態窒素']     # X軸データ
         y = [
-            query.ec, query.nh4n, query.no3n, query.inorganic_nitrogen, query.nh4
+            query.ec, query.nh4n, query.no3n, query.nh4no3, query.nh4_per_nh4no3
         ]  # Y軸データ
         chart = graph.plot_graph("窒素関連（A1をグラフ化したもの。９エリアの平均がいいかも）", x, y)  # グラフ作成
 
