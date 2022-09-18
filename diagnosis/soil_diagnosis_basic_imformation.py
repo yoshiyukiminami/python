8648c71ba6af802b574aaf93519ee05512cedeeb
yoshiyuki minami <minami@agsoil.jp>
2022-09-18 13:36:36 +0900

    #48 基本情報の生成_仕掛り

--- diagnosis/soil_diagnosis_basic_imformation.py
+++ diagnosis/soil_diagnosis_basic_imformation.py
@@ -1,4 +1,4 @@
-# Step-1 soil_chemical_propertiesフォルダにある測定データ（xlsx）を読み込む
+# Step-1 soil_chemical_propertiesフォルダにある測定データ（xlsx）から基本情報を読み込む
 # Step-2 基本情報を抽出し、PPTXのfield_propertiesを生成する
 # Step-3 圃場測定時画像フォルダにある画像データを読み込む
 # Step-4 Step-2のfield_prppertiesに該当画像を追加する
@@ -18,22 +18,44 @@ if __name__ == '__main__':
     files = glob.glob(filedir + '/**/*.xlsx', recursive=True)
     pprint.pprint(files)
 
-    # フォルダにある測定データ（.xlsx）を読み込む
+    # 【Step-1-1】フォルダにある測定データ（.xlsx）から基本情報を読み込む
     for file in files:
         df = pd.read_excel(file, sheet_name='基本情報')
         df = df.loc[:, ['ID', '出荷団体名', '生産者名', '圃場名', '面積（㎡）', '圃場位置(緯度)', '圃場位置(経度)', '品目名', '作型']]
-        # 欠損値の判定
+        # 「土壌化学性データ」シートから必要情報の取得
+        df2 = pd.read_excel(file, sheet_name='土壌化学性データ')
+        df2 = df2.loc[:, ['ID', '採土日', '採土法']]
+        # 「土壌物理性データ」シートから必要情報の取得
+        df3 = pd.read_excel(file, sheet_name='土壌物理性データ')
+        df3 = df3.loc[:, ['ID', '測定日', '測定法', '測定状態']]
+
+        # 【Step-1-2】欠損値の判定（基本情報＝df）と画像タイトルの取得
         isvalid = True
         for i in range(len(df)):
             if df.loc[i].isnull().any():
-                print("欠損値のある行が含まれています")
+                print("基本情報に欠損値のある行が含まれています")
                 isvalid = False
             else:
-                print("データは正常です")
+                print("基本情報のデータは正常です")
                 df_title = df.loc[i]
                 picture_titles = df_title[['ID', '出荷団体名', '生産者名', '圃場名', '品目名', '作型']].values
                 picture_title = '基本情報_' + '_'.join(picture_titles)
-                # print(df2)
-                # print(picture_title)
-                # graphset_2x2(df2, graph_title, hojyomei)
-    # print(df)
+                print(picture_title)
+
+        # 【Step-1-3】欠損値の判定（土壌化学性データ＝df2）と画像タイトルの取得
+        isvalid = True
+        for i in range(len(df2)):
+            if df2.loc[i].isnull().any():
+                print("土壌化学性データに欠損値のある行が含まれています")
+                isvalid = False
+            else:
+                print("土壌化学性データの必要情報は正常です")
+
+        # 【Step-1-4】欠損値の判定（土壌物理性データ＝df3）と画像タイトルの取得
+        isvalid = True
+        for i in range(len(df3)):
+            if df3.loc[i].isnull().any():
+                print("土壌物理性データに欠損値のある行が含まれています")
+                isvalid = False
+            else:
+                print("土壌物理性データの必要情報は正常です")
