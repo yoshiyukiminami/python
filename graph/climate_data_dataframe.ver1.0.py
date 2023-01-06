933a492099790c3d4bc8fc5bda52133f879f9d97
yoshiyuki minami <minami@agsoil.jp>
2023-01-06 19:00:37 +0900

    #54 気象データの元データファイル（CSV）を読み込み、1つのデータファイルにまとめるプログラムは完成 ・データの欠損値（NaN）を補正するプログラムが未完成 ・データから所定のチャート（積算平均気温、積算日照時間）の作成も未着手
    soil・・のpyはプログラム名の変更のみ

---
+++ graph/climate_data_dataframe.ver1.0.py
@@ -0,0 +1,103 @@
+import csv
+import datetime
+import glob
+import os
+import pprint
+import pandas as pd
+from numpy import NaN
+
+
+def data_back_stitch(df_csv, error_data):
+    print(df_csv[error_data], type(df_csv[error_data]))
+    print(df_csv[error_data].isnull())
+    # for i, val in df_csv[error_data].iteritems():
+    #     print(i, val, type(val))
+    #     if val is float('nan'):
+    #         print(i)
+    #     else:
+    #         print("NaNはありません")
+
+
+if __name__ == '__main__':
+    # 定数
+    filedir = 'C:/Users/minam/Desktop/climate_data_dl/'
+    save_filedir = 'C:/Users/minam/Desktop/climate_data_save/'
+
+    # DATAFRAMEの原型alldf（辞書型）
+    alldf = {'観測地点': [], '年月日': [],
+             '平均気温': [], '平均気温（品質）': [], '平均気温（均質）': [],
+             '日照時間': [], '日照時間（品質）': [], '日照時間（均質）': [],
+             '最高気温': [], '最高気温（品質）': [], '最高気温（均質）': [],
+             '最低気温': [], '最低気温（品質）': [], '最低気温（均質）': [],
+             '降水量の合計': [], '降水量の合計（品質）': [], '降水量の合計（均質）': [],
+             '1時間降水量の最大': [], '1時間降水量の最大（品質）': [], '1時間降水量の最大（均質）': []
+             }
+
+    # climate_data_dlフォルダー内にあるファイルをfilesに取得
+    files = glob.glob(filedir + '/*.csv', recursive=True)
+    # ファイル名から観測地点を特定
+    isvalid = True
+    for file in files:
+        with open(file, newline='') as f:
+            reader = csv.reader(f)
+            row_list = [row for row in reader]
+            sokutei_point_list = row_list[2][1:]
+            # 観測地点名の取得とファイルに観測地点が複数ある場合、プログラムを中断する
+            if len(set(sokutei_point_list)) == 1:
+                sokutei_point = sokutei_point_list[0]
+            else:
+                print("ファイルに複数の測定地点があります")
+                isvalid = False
+            data_list = row_list[6:]
+            for data in data_list:
+                alldf['観測地点'].append(sokutei_point)
+                alldf['年月日'].append(datetime.datetime.strptime(data[0], '%Y/%m/%d'))
+                alldf['平均気温'].append(data[1])
+                alldf['平均気温（品質）'].append(data[2])
+                alldf['平均気温（均質）'].append(data[3])
+                alldf['日照時間'].append(data[4])
+                alldf['日照時間（品質）'].append(data[5])
+                alldf['日照時間（均質）'].append(data[6])
+                alldf['最高気温'].append(data[7])
+                alldf['最高気温（品質）'].append(data[8])
+                alldf['最高気温（均質）'].append(data[9])
+                alldf['最低気温'].append(data[10])
+                alldf['最低気温（品質）'].append(data[11])
+                alldf['最低気温（均質）'].append(data[12])
+                alldf['降水量の合計'].append(data[13])
+                alldf['降水量の合計（品質）'].append(data[14])
+                alldf['降水量の合計（均質）'].append(data[15])
+                alldf['1時間降水量の最大'].append(data[16])
+                alldf['1時間降水量の最大（品質）'].append(data[17])
+                alldf['1時間降水量の最大（均質）'].append(data[18])
+
+    df_climate = pd.DataFrame(alldf)
+    # 2つ以上の観測地点が混在している場合に警告、OKの場合はCSVファイルでSAVEホルダーに保存
+    kansoku_point_list = df_climate['観測地点']
+    if not len(set(kansoku_point_list)) == 1:
+        print("警告：観測地点が複数あります。")
+    else:
+        kansoku_point = kansoku_point_list[0]
+        df_climate = df_climate.sort_values(by="年月日")
+        df_climate = df_climate.reset_index(drop=True)
+        df_climate_kion = df_climate[['平均気温', '最高気温', '最低気温', '日照時間']]
+        # 保存ファイル名の生成
+        save_name = save_filedir + '気象元データ_2008-2023_' + kansoku_point + '.csv'
+        print(save_name)
+        df_climate.to_csv(save_name, encoding='Shift-JIS')
+
+    # 空白の補正（平均気温、最高気温、最低気温のみ）
+    # climate_data_saveフォルダー内にあるファイルをfile2に取得
+    files2 = glob.glob(save_filedir + '/*.csv', recursive=True)
+    for file2 in files2:
+        df_csv = pd.read_csv(file2, encoding='Shift-JIS')
+        df_csv_check = df_csv[['平均気温', '最高気温', '最低気温']]
+        df_csv_check = df_csv_check.isnull().any()
+        for index, value in enumerate(df_csv_check):
+            if value is True:
+                error_data = str(df_csv_check.index[index])
+                error_msg1 = error_data + "に欠損値があるため、修正が必要です。"
+                print(error_msg1)
+                data_back_stitch(df_csv, error_data)
+            else:
+                print("欠損値はありません")
