6fb34ba6320f1c3803a14d4a768f0a3f912b021f
yoshiyuki minami <minami@agsoil.jp>
2023-03-04 15:05:52 +0900

    #58 #60 step-4までほぼ完成

--- graph/climate_data_to_analysisi_graph.ver1.0.py
+++ graph/climate_data_to_analysisi_graph.ver1.0.py
@@ -1,38 +1,57 @@
 import csv
 import datetime
 import glob
+import re
+
 import pandas as pd
 import openpyxl
 from dateutil.relativedelta import relativedelta
 from monthdelta import monthmod
 
-def func_get_by_perspective2(df_slice_all, perspectives1, perspectives2):
+def func_get_by_perspective2(df_slice_all, perspectives1, perspectives2, kikan_start):
     # print(df_slice_all)
     df_slice_all['月日'] = df_slice_all['年月日'].dt.strftime('%m-%d')
+    df_slice_all['月日'] = [f"{re.sub('^0[1-3]', f'{12 + int(x[:2])}', x)}" for x in df_slice_all['年月日'].dt.strftime('%m-%d')]
+    df_slice_all['年度'] = [(x - relativedelta(months=3)).strftime('%Y') for x in df_slice_all['年月日']]
+    print("===", df_slice_all)
+    kikan_start_date = kikan_start.strftime('%m-%d')
+    # print(kikan_start_date)
     df_slice_all['年'] = df_slice_all['年月日'].dt.strftime('%Y')
-    print(df_slice_all, df_slice_all['月日'].dtypes)
-    df_slice_perspective = pd.pivot_table(df_slice_all, index='月日', columns='年', values='平均気温')
+    # df_slice_all['No'] = df_slice_all.groupby('年').cumcount()
+    # print(df_slice_all, df_slice_all['月日'].dtypes)
+    df_slice_perspective = pd.pivot_table(df_slice_all, index='月日',
+                                          columns='年度', values=perspectives1)
+    df_slice_perspective.to_csv('aaa.csv', encoding='shift-jis')
     print(df_slice_perspective)
 
 
 def func_get_by_perspective(df_slice_loop, perspectives1, perspectives2):
-    print(df_slice_loop)
+    df_slice_loop = df_slice_loop.reset_index(drop=True)
+    df_slice_loop['月日'] = df_slice_loop['年月日'].dt.strftime('%m-%d')
+    col_name_year = df_slice_loop['年月日'][0].strftime('%Y')
+    # 切り出したDataframeにNoと年（開始日の年→pivot_tableした時のカラム名になる）列を追加
+    df_slice_loop['No'] = range(1, len(df_slice_loop.index) + 1)
+    df_slice_loop['年度'] = col_name_year
+    # print("===", df_slice_loop)
     sokutei_point = df_slice_loop['観測地点'].iloc[0, ]
-    print(sokutei_point)
+    # 測定項目毎に切り出してwide-dataにpivotする
     for perspective in perspectives1:
-        df_perspective = df_slice_loop.loc[:, ['年月日', perspective]]
-        df_perspective_date = df_slice_loop.loc[:, '年月日']
-        # print(df_perspective_date)
-        for date in df_perspective_date:
-            # print(date)
-            date_replace = date.strftime('%m-%d')
-            # print(date_replace, type(date_replace))
-            df_perspective_date.replace(date, date_replace)
-        col_year = df_perspective.iloc[0, 0]
-        col_year = str(col_year.year)
-        col_name = col_year + '_' + perspective
-        print(col_name, type(col_name))
-    # print(df_perspective_date)
+        df_perspective = pd.pivot_table(df_slice_loop, index=['No', '月日'], columns='年度',
+                                        values=perspective)
+        col_rename = df_perspective.columns.values + "_" + str(perspective)
+        col_prename = df_perspective.columns.values[0]
+        col_rename = col_rename[0]
+        df_perspective.rename(columns={col_prename: col_rename}, inplace=True)
+        print(df_perspective)
+        # perspective2にある項目は積算したDataframeを生成する
+        if perspective in perspectives2:
+            df_perspective_sekisan = df_perspective.cumsum()
+            col_prename2 = df_perspective_sekisan.columns.values[0]
+            print(col_prename2)
+            col_rename2 = col_prename2.split('_')
+            # col_rename2 = col_prename2.insert(1, "積算")
+            print(col_rename2)
+            print(df_perspective_sekisan)
 
 
 if __name__ == '__main__':
@@ -54,8 +73,9 @@ if __name__ == '__main__':
     # kikan_end = '2023/2/28'
     # kikan_end = datetime.datetime.strptime(kikan_end, '%Y/%m/%d')
     # グラフ作成する項目を選定
+    # perspective1:グラフ化する項目、perspective2:積算する項目
     perspectives1 = ['平均気温', '日照時間', '最高気温', '最低気温', '降水量の合計', '1時間降水量の最大']
-    perspectives2 = ['積算平均気温', '積算日照時間']
+    perspectives2 = ['平均気温', '日照時間']
     # climate_data_save_hoseiフォルダー内にあるファイルをfilesに取得
     filedir = 'C:/Users/minam/Desktop/climate_data_save_hosei/'
     files = glob.glob(filedir + '/*.csv', recursive=True)
@@ -77,9 +97,9 @@ if __name__ == '__main__':
     isvalid = True
     for file in files:
         with open(file, newline='') as f:
-            df = pd.read_csv(f)
+            df = pd.read_csv(f, index_col=0)
             sokutei_point_list = [col for col in df.iteritems()]
-            sokutei_point_list = sokutei_point_list[1][1:]
+            sokutei_point_list = sokutei_point_list[0][1:]
             # 観測地点名の取得とファイルに観測地点が複数ある場合、プログラムを中断する
             if len(set(sokutei_point_list[0][1:])) == 1:
                 print("観測地点は一つです")
@@ -96,33 +116,32 @@ if __name__ == '__main__':
                                                               '%Y-%m-%d')
                 temp_kikan_end = temp_kikan_start + relativedelta(months=int(kikan_range_month))
                 df_slice_loop = df[df['年月日'].isin(pd.date_range(temp_kikan_start, temp_kikan_end))]
-                # print(df_slice_loop)
                 # 指定期間で抽出したデータから項目別のdataframeを生成する
                 # func_get_by_perspective(df_slice_loop, perspectives1, perspectives2)
 
                 for i, data in enumerate(df_slice_loop.itertuples()):
                     # print(i, "==", data, type(data))
-                    alldf['観測地点'].append(data[2])
+                    alldf['観測地点'].append(data[1])
                     # alldf['年月日'].append(datetime.datetime.strptime(data[2], '%Y-%m-%d'))
-                    alldf['年月日'].append(data[3])
-                    alldf['平均気温'].append(float(data[4]))
-                    alldf['平均気温（品質）'].append(int(data[5]))
-                    alldf['平均気温（均質）'].append(int(data[6]))
-                    alldf['日照時間'].append(float(data[7]))
-                    alldf['日照時間（品質）'].append(int(data[8]))
-                    alldf['日照時間（均質）'].append(int(data[9]))
-                    alldf['最高気温'].append(float(data[10]))
-                    alldf['最高気温（品質）'].append(int(data[11]))
-                    alldf['最高気温（均質）'].append(int(data[12]))
-                    alldf['最低気温'].append(float(data[13]))
-                    alldf['最低気温（品質）'].append(int(data[14]))
-                    alldf['最低気温（均質）'].append(int(data[15]))
-                    alldf['降水量の合計'].append(float(data[16]))
-                    alldf['降水量の合計（品質）'].append(int(data[17]))
-                    alldf['降水量の合計（均質）'].append(int(data[18]))
-                    alldf['1時間降水量の最大'].append(float(data[19]))
-                    alldf['1時間降水量の最大（品質）'].append(int(data[20]))
-                    alldf['1時間降水量の最大（均質）'].append(int(data[21]))
+                    alldf['年月日'].append(data[2])
+                    alldf['平均気温'].append(float(data[3]))
+                    alldf['平均気温（品質）'].append(int(data[4]))
+                    alldf['平均気温（均質）'].append(int(data[5]))
+                    alldf['日照時間'].append(float(data[6]))
+                    alldf['日照時間（品質）'].append(int(data[7]))
+                    alldf['日照時間（均質）'].append(int(data[8]))
+                    alldf['最高気温'].append(float(data[9]))
+                    alldf['最高気温（品質）'].append(int(data[10]))
+                    alldf['最高気温（均質）'].append(int(data[11]))
+                    alldf['最低気温'].append(float(data[12]))
+                    alldf['最低気温（品質）'].append(int(data[13]))
+                    alldf['最低気温（均質）'].append(int(data[14]))
+                    alldf['降水量の合計'].append(float(data[15]))
+                    alldf['降水量の合計（品質）'].append(int(data[16]))
+                    alldf['降水量の合計（均質）'].append(int(data[17]))
+                    alldf['1時間降水量の最大'].append(float(data[18]))
+                    alldf['1時間降水量の最大（品質）'].append(int(data[19]))
+                    alldf['1時間降水量の最大（均質）'].append(int(data[20]))
             df_slice_all = pd.DataFrame(alldf)
             # df_slice_all.to_csv('df_slice_all.csv', encoding='shift-jis')
-            func_get_by_perspective2(df_slice_all, perspectives1, perspectives2)
+            func_get_by_perspective2(df_slice_all, perspectives1, perspectives2, kikan_start)
