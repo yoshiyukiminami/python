c876532556f24aa4292d5c5b300d316169a0990e
yoshiyuki minami <minami@agsoil.jp>
2023-03-19 01:10:41 +0900

    #58 #60 step-5までOK　step-6から途中仕掛り         defの引数整理（シャドーイングおこらないように）         業務内容ごとにdefで小分けする         ・・途中まで

--- graph/climate_data_to_analysisi_graph.ver1.0.py
+++ graph/climate_data_to_analysisi_graph.ver1.0.py
@@ -16,59 +16,75 @@ def func_get_by_perspective(df1: pd.DataFrame, k1: list, k2: list, month: int):
         seiki_hyogen = "^0" + "[" + "1-" + str(month + 1) + "]"
         df1['月日'] = [f"{re.sub(seiki_hyogen, f'{12 + int(x[:month])}', x)}" for x in df1['年月日'].dt.strftime('%m-%d')]
         # 年をまたぐ期間設定対策2として、1月以降が入る期間設定の場合、1月以降のデータも年度としては12月までの年度に含む
-        df1['年度'] = [(x - relativedelta(months=i)).strftime('%Y') for x in df1['年月日']]
+        df1['年度'] = [(x - relativedelta(months=month + 1)).strftime('%Y') for x in df1['年月日']]
         print("年またぎ有り")
     else:
         # dfの「年月日」列から月日に変換した列を追加
         df1['年度'] = df['年月日'].dt.strftime('%Y')
         print("年またぎ無し")
     # df_slide_allをpivot_tableに変換（wide型）
-    df_slice_perspective = pd.pivot_table(df1, index='月日', columns='年度', values=k1)
+    df1 = pd.pivot_table(df1, index='月日', columns='年度', values=k1)
     # 月日表示を元に戻す際の昇順崩れ防止に1から連番を降った新しい列（列名：No）を追加し、先頭列に挿入する
-    new_col_no = pd.Series(range(1, len(df_slice_perspective.index) + 1))
-    df_slice_perspective.insert(0, 'No', new_col_no)
-    func_get_by_perspective2(df_slice_perspective, k1, k2)
+    new_col_no = pd.Series(range(1, len(df1.index) + 1), index=df1.index)
+    df1.insert(0, 'No', new_col_no)
+    func_get_by_perspective2(df1, k1, k2)
 
 
 def func_get_by_perspective2(df2: pd.DataFrame, k1: list, k2: list):
     # step-5:k2リストの項目のみ、積算演算した列を追加する
-    # df2のカラム名（マルチカラムのlevel=1）から年度のリスト(nendo_list)を生成する
-    nendo_list = set(df2.columns.droplevel(level=0))
-    nendo_list = [x for x in nendo_list if x]
-    # nendo_listを昇順（年度の古いが先）に並び替える
-    for y in nendo_list:
+    # df2のカラム名（マルチカラムのlevel=1）から年度のリスト(year_list)を生成する
+    year_list = set(df2.columns.droplevel(level=0))
+    year_list = [x for x in year_list if x]
+    # year_listを昇順（年度の古いが先）に並び替える
+    for y in year_list:
         y = int(y)
-    nendo_list.sort()
+    year_list.sort()
     # df_slice_perspectiveに積算したperspective2の項目の列を追加する
     for z in k2:
-        for y in nendo_list:
+        for y in year_list:
             new_col_name = '積算' + str(z)
-            df2[(new_col_name, z)] = df2[z, y].cumsum()
-    func_get_by_perspective3_prepare(k1, k2, nendo_list)
+            df2[(new_col_name, y)] = df2[z, y].cumsum()
+    func_get_by_perspective3(df2, k1, k2, year_list)
 
 
-def func_get_by_perspective3_prepare(k1: list, k2: list, nendo_list: list):
-    # step-6:各項目の特定期間の平均を算出する・・事前準備（特定期間の設定）
-    # 比較した年度・期間の設定
-    # ①比較したい期間の初年度
-    compare_year_start1 = '2008'
-    compare_year_end1 = '2020'
-
-    year_ave_list = []
-    for i in range(2008, 2021):
-        year_ave_list.append(('平均気温', i))
-    print(year_ave_list)
-
-
-
-    g = df3.groupby(['月日'])[[('平均気温', '2018'), ('平均気温', '2019'), ('平均気温', '2020')]].mean()
-    # print(g.apply(lambda a: a[:]), type(g))
-    # print(g.mean(axis=1))
-    df3[('平均気温_暖冬平均', '2018-2020')] = g.mean(axis=1)
-    # Dataframeの数値を小数点以下1桁に揃える
-    pd.options.display.float_format = '{:.1f}'.format
+def func_get_by_perspective3(df3: pd.DataFrame, k1: list, k2: list, year_list: list):
+    # step-6:各項目の特定期間の平均を算出する（比較データ列の作成）
+    for i in k1:
+        compare_term1, compare_term1_start, compare_term1_end = func_get_by_perspective3_prepare1(i, year_list)
+        new_col_name = (i + "_比較期間_A", str(compare_term1_start + '-' + compare_term1_end))
+        g = df3.groupby(['月日'])[compare_term1].mean()
+        # print(g.apply(lambda a: a[:]), type(g))
+        # print(g.mean(axis=1))
+        df3[new_col_name] = g.mean(axis=1)
+        # Dataframeの数値を小数点以下1桁に揃える
+        # todo:表示変化せず
+        pd.options.display.float_format = '{:.1f}'.format
     # print(df3)
-    df3.to_csv('bbb.csv', encoding='shift-jis', index_label=None)
+    df3.to_csv('bbb.csv', encoding='shift-jis')
+
+
+def func_get_by_perspective3_prepare1(koumoku_basic: list, year: list):
+    # 比較データ列の設定期間を決める関数
+    # 設定期間①：暖冬シーズン
+    # 比較年の開始日・終了日の設定・・アナログ
+    compare_term1_start = '2018'
+    compare_term1_end = '2020'
+    # 設定年のエラー検知（Dataframeにない年度の選択、無効な設定期間：開始年の方が新しいや同じ年の選択）
+    if compare_term1_start not in year:
+        print("開始年が対象年にありませんので、以下のリストから再選択してください。")
+        print(year)
+    else:
+        if compare_term1_end not in year:
+            print("終了年が対象年にありませんので、以下のリストから再選択してください。")
+            print(year)
+        else:
+            if int(compare_term1_end) - int(compare_term1_start) <= 0:
+                print("開始年と終了年が同じか、無効な期間設定になっています。")
+            else:
+                compare_term1 = []
+                for j in range(int(compare_term1_start), int(compare_term1_end) + 1):
+                    compare_term1.append((koumoku_basic, str(j)))
+                return compare_term1, compare_term1_start, compare_term1_end
 
 
 if __name__ == '__main__':
@@ -108,7 +124,6 @@ if __name__ == '__main__':
              '降水量の合計': [], '降水量の合計（品質）': [], '降水量の合計（均質）': [],
              '1時間降水量の最大': [], '1時間降水量の最大（品質）': [], '1時間降水量の最大（均質）': []
              }
-
     # ファイル名から観測地点を特定
     # todo:観測地点（例：菊川牧之原）が2つ以上のファイルに含まれていた場合の処理
     # ヒント：読み込んだCSVファイルを一つのdataframeにまとめて（concat)エラーチェックをする
@@ -134,7 +149,7 @@ if __name__ == '__main__':
                                                               '%Y-%m-%d')
                 temp_kikan_end = temp_kikan_start + relativedelta(months=int(kikan_range_month))
                 df_slice_loop = df[df['年月日'].isin(pd.date_range(temp_kikan_start, temp_kikan_end))]
-                # 年毎に取得したデータをall_dfのDataframeにappendする
+                # 年毎に取得したデータをall_dfのDataframeに追加していく
                 for i, data in enumerate(df_slice_loop.itertuples()):
                     # print(i, "==", data, type(data))
                     alldf['観測地点'].append(data[1])
@@ -158,7 +173,9 @@ if __name__ == '__main__':
                     alldf['1時間降水量の最大'].append(float(data[18]))
                     alldf['1時間降水量の最大（品質）'].append(int(data[19]))
                     alldf['1時間降水量の最大（均質）'].append(int(data[20]))
+            # すべてを取得した辞書型データフレームのalldfをdf_slice_allというDataframeに変換する
             df_slice_all = pd.DataFrame(alldf)
+            # 確認用のcsv変換（df_slice_all.csv）
             # df_slice_all.to_csv('df_slice_all.csv', encoding='shift-jis')
             # 任意の期間で取り出したDataframe（df_slice_all）を項目別・年度別でピボットテーブルを生成・・関数
             func_get_by_perspective(df_slice_all, perspectives1, perspectives2, over_month)
