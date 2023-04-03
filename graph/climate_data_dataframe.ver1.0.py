870e214eeaddacc851be4570e21955713b943cff
yoshiyuki minami <minami@agsoil.jp>
2023-04-04 08:38:04 +0900

    #654　プログラムの目的文書を追加（1，2行目） 観測地点による出力データの項目数が違う点を改良 Chain‐indexing警告の解除（201行目）

--- graph/climate_data_dataframe.ver1.0.py
+++ graph/climate_data_dataframe.ver1.0.py
@@ -1,5 +1,6 @@
 # このプログラムは、①気象庁HPよりダウンロードされた観測地点別・期間別データをひとつのDataframeにまとめる
 # ②データのないものを検索して補正（数値はなだらかな変化量計算して代入、品質・均質情報は前後の数値を代入）する
+# 追加（20230402）観測地点によりデータ種類が変わる問題への対応・・気象台では一部の項目に「現象なし情報」が追加される。
 import csv
 import datetime
 import glob
@@ -18,12 +19,12 @@ def back_stitch3(line: list, line_name: str, offset: int = 0) -> list:
     if offset != len(line):
         idx = {}  # [平均材料1, 修正パンチイン, 修正パンチアウト, 平均材料2]
         punch_in = False
-        for col, value in enumerate(line[offset::]):
-            if not punch_in and np.isnan(value):
+        for col, val in enumerate(line[offset::]):
+            if not punch_in and np.isnan(val):
                 idx["mean1"] = offset + col - 1
                 idx["punch_in"] = offset + col
                 punch_in = True
-            if punch_in and not np.isnan(value):
+            if punch_in and not np.isnan(val):
                 idx["punch_out"] = offset + col - 1
                 idx["mean2"] = offset + col
                 # print(idx, "idx")
@@ -32,8 +33,8 @@ def back_stitch3(line: list, line_name: str, offset: int = 0) -> list:
                     # print(how_many_times, "how_many_times")
                     tolerance = np.linspace(line[idx["mean1"]], line[idx["mean2"]], how_many_times)
                     # print(tolerance, "tolerance")
-                    for i, n_value in enumerate(tolerance):
-                        line[idx["mean1"] + i] = n_value
+                    for n, n_value in enumerate(tolerance):
+                        line[idx["mean1"] + n] = n_value
                     line = back_stitch3(line, line_name, idx["mean2"])
                     break
                 elif line_name == '平均気温（品質）' or line_name == '平均気温（均質）':
@@ -80,21 +81,25 @@ def find_invalid_data(file2: str):
             records.append(back_stitch3(line, line_name))
     return records, records2, df_csv
 
+
 if __name__ == '__main__':
     # 定数
     filedir = 'C:/Users/minam/Desktop/climate_data_dl/'
     save_filedir = 'C:/Users/minam/Desktop/climate_data_save/'
     save_hosei_filedir = 'C:/Users/minam/Desktop/climate_data_save_hosei/'
 
-    # DATAFRAMEの原型alldf（辞書型）
-    alldf = {'観測地点': [], '年月日': [],
-             '平均気温': [], '平均気温（品質）': [], '平均気温（均質）': [],
-             '日照時間': [], '日照時間（品質）': [], '日照時間（均質）': [],
-             '最高気温': [], '最高気温（品質）': [], '最高気温（均質）': [],
-             '最低気温': [], '最低気温（品質）': [], '最低気温（均質）': [],
-             '降水量の合計': [], '降水量の合計（品質）': [], '降水量の合計（均質）': [],
-             '1時間降水量の最大': [], '1時間降水量の最大（品質）': [], '1時間降水量の最大（均質）': []
-             }
+    # 測定データを格納する辞書dict_all
+    dict_all = {'観測地点': [], '年月日': [],
+                '平均気温': [], '平均気温（品質）': [], '平均気温（均質）': [],
+                '日照時間': [], '日照時間（現象なし情報）': [], '日照時間（品質）': [],
+                '日照時間（均質）': [],
+                '最高気温': [], '最高気温（品質）': [], '最高気温（均質）': [],
+                '最低気温': [], '最低気温（品質）': [], '最低気温（均質）': [],
+                '降水量の合計': [], '降水量の合計（現象なし情報）': [], '降水量の合計（品質）': [],
+                '降水量の合計（均質）': [],
+                '1時間降水量の最大': [], '1時間降水量の最大（現象なし情報）': [], '1時間降水量の最大（品質）': [],
+                '1時間降水量の最大（均質）': []
+                }
 
     # climate_data_dlフォルダー内にあるファイルをfilesに取得
     files = glob.glob(filedir + '/*.csv', recursive=True)
@@ -105,37 +110,71 @@ if __name__ == '__main__':
         with open(file, newline='') as f:
             reader = csv.reader(f)
             row_list = [row for row in reader]
-            sokutei_point_list = row_list[2][1:]
+            kansoku_point_list = row_list[2][1:]
             # 観測地点名の取得とファイルに観測地点が複数ある場合、プログラムを中断する
-            if len(set(sokutei_point_list)) == 1:
-                sokutei_point = sokutei_point_list[0]
-            else:
+            if not len(set(kansoku_point_list)) == 1:
                 print("ファイルに複数の測定地点があります")
                 isvalid = False
-            data_list = row_list[6:]
-            for data in data_list:
-                alldf['観測地点'].append(sokutei_point)
-                alldf['年月日'].append(datetime.datetime.strptime(data[0], '%Y/%m/%d'))
-                alldf['平均気温'].append(data[1])
-                alldf['平均気温（品質）'].append(data[2])
-                alldf['平均気温（均質）'].append(data[3])
-                alldf['日照時間'].append(data[4])
-                alldf['日照時間（品質）'].append(data[5])
-                alldf['日照時間（均質）'].append(data[6])
-                alldf['最高気温'].append(data[7])
-                alldf['最高気温（品質）'].append(data[8])
-                alldf['最高気温（均質）'].append(data[9])
-                alldf['最低気温'].append(data[10])
-                alldf['最低気温（品質）'].append(data[11])
-                alldf['最低気温（均質）'].append(data[12])
-                alldf['降水量の合計'].append(data[13])
-                alldf['降水量の合計（品質）'].append(data[14])
-                alldf['降水量の合計（均質）'].append(data[15])
-                alldf['1時間降水量の最大'].append(data[16])
-                alldf['1時間降水量の最大（品質）'].append(data[17])
-                alldf['1時間降水量の最大（均質）'].append(data[18])
-
-    df_climate = pd.DataFrame(alldf)
+            else:
+                kansoku_point = kansoku_point_list[0]
+                # 観測地点が気象台かアメダス化を項目数（気象台：21、アメダス：18）で判定
+                koumoku_number = len(kansoku_point_list)
+                print(koumoku_number)
+                data_list = row_list[6:]
+                if koumoku_number == 21:
+                    print("観測地点は気象台です")
+                    for data in data_list:
+                        # print(data)
+                        dict_all['観測地点'].append(kansoku_point)
+                        dict_all['年月日'].append(datetime.datetime.strptime(data[0], '%Y/%m/%d'))
+                        dict_all['平均気温'].append(data[1])
+                        dict_all['平均気温（品質）'].append(data[2])
+                        dict_all['平均気温（均質）'].append(data[3])
+                        dict_all['日照時間'].append(data[4])
+                        dict_all['日照時間（現象なし情報）'].append(data[5])
+                        dict_all['日照時間（品質）'].append(data[6])
+                        dict_all['日照時間（均質）'].append(data[7])
+                        dict_all['最高気温'].append(data[8])
+                        dict_all['最高気温（品質）'].append(data[9])
+                        dict_all['最高気温（均質）'].append(data[10])
+                        dict_all['最低気温'].append(data[11])
+                        dict_all['最低気温（品質）'].append(data[12])
+                        dict_all['最低気温（均質）'].append(data[13])
+                        dict_all['降水量の合計'].append(data[14])
+                        dict_all['降水量の合計（現象なし情報）'].append(data[15])
+                        dict_all['降水量の合計（品質）'].append(data[16])
+                        dict_all['降水量の合計（均質）'].append(data[17])
+                        dict_all['1時間降水量の最大'].append(data[18])
+                        dict_all['1時間降水量の最大（現象なし情報）'].append(data[19])
+                        dict_all['1時間降水量の最大（品質）'].append(data[20])
+                        dict_all['1時間降水量の最大（均質）'].append(data[21])
+                else:
+                    print("観測地点はアメダスです")
+                    for data in data_list:
+                        dict_all['観測地点'].append(kansoku_point)
+                        dict_all['年月日'].append(datetime.datetime.strptime(data[0], '%Y/%m/%d'))
+                        dict_all['平均気温'].append(data[1])
+                        dict_all['平均気温（品質）'].append(data[2])
+                        dict_all['平均気温（均質）'].append(data[3])
+                        dict_all['日照時間'].append(data[4])
+                        dict_all['日照時間（現象なし情報）'].append(None)
+                        dict_all['日照時間（品質）'].append(data[5])
+                        dict_all['日照時間（均質）'].append(data[6])
+                        dict_all['最高気温'].append(data[7])
+                        dict_all['最高気温（品質）'].append(data[8])
+                        dict_all['最高気温（均質）'].append(data[9])
+                        dict_all['最低気温'].append(data[10])
+                        dict_all['最低気温（品質）'].append(data[11])
+                        dict_all['最低気温（均質）'].append(data[12])
+                        dict_all['降水量の合計'].append(data[13])
+                        dict_all['降水量の合計（品質）'].append(data[14])
+                        dict_all['降水量の合計（現象なし情報）'].append(None)
+                        dict_all['降水量の合計（均質）'].append(data[15])
+                        dict_all['1時間降水量の最大'].append(data[16])
+                        dict_all['1時間降水量の最大（現象なし情報）'].append(None)
+                        dict_all['1時間降水量の最大（品質）'].append(data[17])
+                        dict_all['1時間降水量の最大（均質）'].append(data[18])
+    df_climate = pd.DataFrame(dict_all)
     # 2つ以上の観測地点が混在している場合に警告（処理中断）、OKの場合はCSVファイルでSAVEホルダーに保存
     kansoku_point_list = df_climate['観測地点']
     if not len(set(kansoku_point_list)) == 1:
@@ -148,22 +187,21 @@ if __name__ == '__main__':
         # 保存ファイル名の生成
         save_name = save_filedir + '気象元データ_2008-2023_' + kansoku_point + '.csv'
         df_climate.to_csv(save_name, encoding='Shift-JIS', index=False)
+        # 空白の補正（平均気温、最高気温、最低気温とその品質・均質情報のみ）
+        # climate_data_saveフォルダー内にあるファイルをfile2に取得
+        files2 = glob.glob(save_filedir + '/*.csv', recursive=True)
+        for file2 in files2:
+            records, records2, df_csv = find_invalid_data(file2)
+            df_climate_exchange = pd.DataFrame(records).T.set_axis(records2, axis='columns')
+            # print(df_csv, "処理対象")
+            # print(df_climate_exchange)
+            for target_column in df_csv.columns.intersection(df_climate_exchange.columns):
+                # print(target_column, "更新中")
+                for i, value in enumerate(df_csv[target_column]):
+                    df_csv.loc[i, target_column] = df_climate_exchange.loc[i, target_column]
+                    # print(df_csv[target_column][i], "置換結果")
+            # df_csv.to_csv('df_csv_exchange.csv', encoding='Shift-JIS')
 
-    # 空白の補正（平均気温、最高気温、最低気温とその品質・均質情報のみ）
-    # climate_data_saveフォルダー内にあるファイルをfile2に取得
-    files2 = glob.glob(save_filedir + '/*.csv', recursive=True)
-    for file2 in files2:
-        records, records2, df_csv = find_invalid_data(file2)
-        df_climate_exchange = pd.DataFrame(records).T.set_axis(records2, axis='columns')
-        # print(df_csv, "処理対象")
-        # print(df_climate_exchange)
-        for target_column in df_csv.columns.intersection(df_climate_exchange.columns):
-            # print(target_column, "更新中")
-            for i, value in enumerate(df_csv[target_column]):
-                df_csv[target_column][i] = df_climate_exchange[target_column][i]
-                # print(df_csv[target_column][i], "置換結果")
-        # df_csv.to_csv('df_csv_exchange.csv', encoding='Shift-JIS')
-
-        save_name_hosei = save_hosei_filedir + '気象元データ_2008-2023_' + kansoku_point + '_hosei.csv'
-        df_csv.to_csv(save_name_hosei, encoding='shift-jis', index=True)
-        print('欠損値修正版の出力が完了しました')
+            save_name_hosei = save_hosei_filedir + '気象元データ_2008-2023_' + kansoku_point + '_hosei.csv'
+            df_csv.to_csv(save_name_hosei, encoding='shift-jis', index=True)
+            print('欠損値修正版の出力が完了しました')
