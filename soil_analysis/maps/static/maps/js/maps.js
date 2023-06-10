b08dbb5b264e2cf665f569909a853eaa909255ee
yoshi <yoshitakaOkada0214@gmail.com>
2023-06-10 21:00:25 +0900

    polygon書けるようになった

--- soil_analysis/maps/static/maps/js/maps.js
+++ soil_analysis/maps/static/maps/js/maps.js
@@ -12,36 +12,39 @@ function initMap() {
     try {
         var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
         addPinsToMap(map, locationPins.formatted_pins);
+        addPinsToMapPolygon(map, locationPins.formatted_pins);
     }
     catch (error) {
         console.error('JSON parsing error:', error);
     }
 }
 function addPinsToMap(map, pins) {
-    if (pins && Array.isArray(pins)) {
-        pins.forEach(function (pin) {
-            var location = pin.geometry.location;
-            var latLng = new google.maps.LatLng(location.lat, location.lng);
-            new google.maps.Marker({
-                position: latLng,
-                map: map,
-                title: pin.name
-            });
+    pins.forEach(function (pin) {
+        var location = pin.geometry.location;
+        var latLng = new google.maps.LatLng(location.lat, location.lng);
+        new google.maps.Marker({
+            position: latLng,
+            map: map,
+            title: pin.name
         });
-    }
+    });
 }
-// function addPinsToMapPolygon(map: HTMLElement, pins: any[]): void {
-//   const bermudaTriangle = new google.maps.Polygon({
-//     paths: triangleCoords,
-//     strokeColor: "#FF0000",
-//     strokeOpacity: 0.8,
-//     strokeWeight: 2,
-//     fillColor: "#FF0000",
-//     fillOpacity: 0.35,
-//   });
-//
-//   bermudaTriangle.setMap(map);
-// }
+function addPinsToMapPolygon(map, pins) {
+    var coords = [];
+    pins.forEach(function (pin) {
+        var location = pin.geometry.location;
+        coords.push({ lat: location.lat, lng: location.lng });
+    });
+    var polygon = new google.maps.Polygon({
+        paths: coords,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: "#FF0000",
+        fillOpacity: 0.35,
+    });
+    polygon.setMap(map);
+}
 // this is googlemap's callback
 function mapLoaded() {
     initMap();
