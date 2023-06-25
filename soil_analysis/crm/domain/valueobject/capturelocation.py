4c2b882f8c282729b8ab53cc73e235dbf3136bd5
yoshi <yoshitakaOkada0214@gmail.com>
2023-06-25 15:43:17 +0900

    test ok

---
+++ soil_analysis/crm/domain/valueobject/capturelocation.py
@@ -0,0 +1,46 @@
+import math
+
+from soil_analysis.crm.domain.valueobject.coordinates import Coordinates
+
+
+class CaptureLocation:
+    """
+    Note: 右に圃場１、左に圃場2がある状態で、それら圃場を手前から、かつ圃場の左右の真ん中から撮影したとき、撮影座標との距離の関係性は二等辺三角形になる
+      それでは圃場が特定できないので、撮影した方位角（例えば圃場２）に向かって撮影座標を10m補正できれば、圃場１からは少し遠くなり、圃場２へは少し近くなる
+      そんな下準備をして find_nearest_land で処理できれば、写真から圃場を特定できる
+    """
+    def __init__(self, longitude, latitude, azimuth):
+        self._coordinates_origin = Coordinates(f"{longitude},{latitude}")
+        self._coordinates = self._move(azimuth)
+
+    def _move(self, azimuth: float, distance: float = 0.01):
+        """
+        緯度・経度座標を基に、指定された方位角と距離で移動した座標を計算します。
+
+        :param azimuth: 方位角（単位: 度）
+        :param distance: 移動距離（単位: キロメートル）
+        :return: 移動後の座標を表す Coordinates オブジェクト
+        """
+        origin_latitude, origin_longitude = self._coordinates_origin.get_lat_lng()
+
+        # 角度をラジアンに変換
+        azimuth_rad = math.radians(azimuth)
+        # 緯度をラジアンに変換
+        latitude_rad = math.radians(origin_latitude)
+        # 赤道半径（地球の半径）を設定
+        radius = 6371.0  # 地球の半径（キロメートル）
+
+        # 目的地までの変位の緯度変化を計算
+        delta_latitude = distance / radius * math.cos(azimuth_rad)
+        # 目的地までの変位の経度変化を計算
+        delta_longitude = distance / (radius * math.sin(latitude_rad)) * math.sin(azimuth_rad)
+
+        # 目的地の緯度を計算
+        destination_latitude = origin_latitude + math.degrees(delta_latitude)
+        # 目的地の経度を計算
+        destination_longitude = origin_longitude + math.degrees(delta_longitude)
+
+        return Coordinates(f"{destination_longitude},{destination_latitude}")
+
+    def get_coordinates(self):
+        return self._coordinates
---
+++ soil_analysis/crm/tests/domain/valueobject/test_capturelocation.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from soil_analysis.crm.domain.valueobject.capturelocation import CaptureLocation
+
+
+class TestCaptureLocation(TestCase):
+    def test_capture_location(self):
+        """
+        Note: テスト要件 座標はxarvioベース(lng,lat)で記す
+          撮影位置の座標は 137.6492809,34.743865 とする（ススムA2とススムA3の中点を googlemap で調べた）
+          撮影位置の座標から、ススムA3の中心点（137.64910916605962,34.74362556670041）に向かって、
+          言い換えると方位角190に向かって10m進んだ先の座標は約 137.64923,34.74378 となるはずである
+          ※補正後座標を算出してみて10m先の座標をgooglemapにおいてみてフィードバックを繰り返した
+        """
+        # 撮影位置の座標と方位角
+        capture_point_lng = 137.6492809
+        capture_point_lat = 34.743865
+        capture_point_azimuth = 190
+
+        # 撮影位置の情報で、CaptureLocationを生成
+        capture_location = CaptureLocation(capture_point_lng, capture_point_lat, capture_point_azimuth)
+
+        print(f'gmap検証用_撮影位置: 34.743865,137.6492809')
+        print(f'gmap検証用_10m先の位置: {capture_location.get_coordinates().get_lat_lng()}')
+        self.assertAlmostEqual(137.64923, capture_location.get_coordinates().get_lng_lat()[0], delta=0.0003)
+        self.assertAlmostEqual(34.74378, capture_location.get_coordinates().get_lng_lat()[1], delta=0.0003)
