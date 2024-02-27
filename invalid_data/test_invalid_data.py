891775be11591a4ee2f9df074bd1b1c44f1650b9
yoshi <yoshitakaOkada0214@gmail.com>
2024-02-28 08:07:32 +0900

    fix: test

--- invalid_data/test_invalid_data.py
+++ invalid_data/test_invalid_data.py
@@ -3,38 +3,54 @@ import numpy as np
 
 from invalid_data import find_spike_point_in_line, average_fill, linear_fill
 
+INVALID_DATA_VALUE = 232
+
 
 class TestGetCursorOfSpike(TestCase):
     def test_get_cursor_of_spike_no_inverse_no_offset(self):
-        result = find_spike_point_in_line([0, 1, 2, 3, 4, 5, 5, 7, 8, 9], 4, (0, 9))
+        result = find_spike_point_in_line([232, 232, 232, 232, 232, 300, 301, 405, 350, 400], INVALID_DATA_VALUE,
+                                          (0, 9))
         self.assertEqual(5, result)
 
     def test_get_cursor_of_spike_inverse_no_offset(self):
-        result = find_spike_point_in_line([1, 2, 3, 4, 5, 7, 6, 5, 4, 3], 4, (0, 9), reverse=True)
+        result = find_spike_point_in_line([232, 232, 232, 232, 300, 301, 405, 350, 232, 232], INVALID_DATA_VALUE,
+                                          (0, 9), reverse=True)
         self.assertEqual(7, result)
 
     def test_get_cursor_of_spike_no_inverse_with_offset(self):
-        result = find_spike_point_in_line(['属性', '属性', 3, 4, 5, 5, 4, 3, 2, '属性'], 4, process_range=(2, 8))
+        result = find_spike_point_in_line(['属性', '属性', 232, 232, 301, 405, 232, 232, 232, np.nan], INVALID_DATA_VALUE,
+                                          process_range=(2, 8))
         self.assertEqual(2, result)
 
     def test_get_cursor_of_spike_inverse_with_offset(self):
-        result = find_spike_point_in_line(['属性', '属性', 3, 4, 5, 5, 4, 3, 2, '属性'], 4, process_range=(2, 8), reverse=True)
+        result = find_spike_point_in_line(['属性', '属性', 232, 232, 301, 405, 232, 232, 232, np.nan], INVALID_DATA_VALUE,
+                                          process_range=(2, 8),
+                                          reverse=True)
         self.assertEqual(3, result)
 
     def test_get_cursor_of_spike_no_spike(self):
-        result = find_spike_point_in_line([1, 2, 3, 4, 5, 5, 4, 3, 2, 1], 6, (0, 9))
+        result = find_spike_point_in_line([232, 232, 232, 232, 232, 232, 232, 232, 232, 232], INVALID_DATA_VALUE,
+                                          process_range=(0, 9))
         self.assertEqual(None, result)
 
 
 class TestAverageFill(TestCase):
-    def test_average_fill_normal(self):
+    def test_average_fill_one_zone_232(self):
         """
-        通常のケースでの'average_fill'のテスト
+        232値を含むゾーンが１つ存在するシナリオで、'average_fill'関数をテストします
         """
         data = [1334, 232, 232, 1360]
-        threshold = 232
-        expected = [1334, 1347, 1347, 1360]
-        result = average_fill(data, threshold)
+        expected = [1334, 1347.0, 1347.0, 1360]
+        result = average_fill(data, INVALID_DATA_VALUE)
+        self.assertEqual(expected, result)
+
+    def test_average_fill_some_zone_232(self):
+        """
+        232値を含むゾーンが複数存在するシナリオで、'average_fill'関数をテストします
+        """
+        data = [1334, 232, 232, 1360, 232, 232, 1400]
+        expected = [1334, 1347.0, 1347.0, 1360, 1380.0, 1380.0, 1400]
+        result = average_fill(data, INVALID_DATA_VALUE)
         self.assertEqual(expected, result)
 
 
