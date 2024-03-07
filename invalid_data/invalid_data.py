ba9dfaff265ebbb580b31d9c7ebe7c55078d6ff9
yoshi <yoshitakaOkada0214@gmail.com>
2024-03-07 23:11:13 +0900

    add value object HardPanCursor

--- invalid_data/invalid_data.py
+++ invalid_data/invalid_data.py
@@ -37,9 +37,20 @@ class HardPanRange(DataRange):
         return f"HardPanRange(start={self.start_pos}, end={self.end_pos}, length={self.length})"
 
 
+class HardPanCursor:
+    def __init__(self, value):
+        if not (1 <= value <= 60):
+            raise ValueError("Value must be within the range 1-60.")
+        self.value = value
+
+    def __str__(self):
+        return f"圧力[kPa]{self.value}cm"
+
+
 class RangeExtractor:
-    def __init__(self, series: pd.Series):
+    def __init__(self, series: pd.Series, hard_pan_cursor: HardPanCursor):
         self.raw = series
+        self.hard_pan_cursor = hard_pan_cursor
         self.numeric_range = None
         self.punch_range = None
         self.hard_pan_range = None
@@ -95,6 +106,12 @@ class RangeExtractor:
                 return i
         return None
 
+    def is_hard_pan(self) -> bool:
+        if self.hard_pan_range:
+            return self.hard_pan_range.start_pos < self.raw.index.get_loc(str(self.hard_pan_cursor))
+
+        return False
+
 
 class FillingIndexes:
     def __init__(self, part1_of_mean: int = 0, invalid_start: int = 0, invalid_end: int = 0, part2_of_mean: int = 0):
@@ -103,70 +120,64 @@ class FillingIndexes:
         self.invalid_end = invalid_end
         self.part2_of_mean = part2_of_mean
 
+    def __str__(self):
+        return (f"part1_of_mean: {self.part1_of_mean}, self.invalid_start: {self.invalid_start}, "
+                f"self.invalid_end: {self.invalid_end}, part2_of_mean: {self.part2_of_mean}")
 
-def average_fill(line: list, start_position: int = 0) -> list:
+
+def average_fill(line: list, recur_start_position: int) -> list:
     """
-    :param line: 数値のリストで表現されたデータ行
-    :param start_position: 無効な値のチェックを開始する行の開始位置
-    :return: 無効な値が周りの有効な値の平均で置き換えられ、修正された数値のリスト
+    与えられた範囲を繰り返し、無効な値を平均に補間された値で置き換えます
+    欠損値: INVALID_DATA_VALUE
 
-    このメソッドは、数値のリストとして表現されたデータ行を取り、無効な値（閾値を超える値）を周囲の有効な値の平均で置き換えます。閾値は値が無効であるかどうかを判断するために使用されます。
-    start_positionが指定されている場合、その行のインデックスから無効な値のチェックを開始します。
-
-    このメソッドは、行内の複数の無効な値を処理するために再帰を使用します。無効な値が見つかると、メソッドは前後の有効な値の平均を計算し、
-    その無効な値を計算した平均で置き換えます。次に、それ自体を再帰的に呼び出し、置き換えた値のインデックスから開始して、さらに無効な値をチェックします。
-    このメソッドは、すべての無効な値がその平均で置き換えられ、修正された数値のリストを返します。
-
-    Example usage:
-        line = [1334, 232, 232, 1360]
-        result = average_fill(line)
-        print(result)  # Output: [1334, 1347.0, 1347.0, 1360]
+    :param line:
+    :param recur_start_position:
+    :return: 平均に埋められた値を表すリスト
     """
-    if start_position is not None:
-        idx = FillingIndexes()
-        punch_in = False
-        for col, value in enumerate(line[start_position::]):
-            if not punch_in and value == INVALID_DATA_VALUE:
-                idx.part1_of_mean = start_position + col - 1
-                idx.invalid_start = start_position + col
-                punch_in = True
-            if punch_in and value > INVALID_DATA_VALUE:
-                idx.invalid_end = start_position + col - 1
-                idx.part2_of_mean = start_position + col
-                for j in range(idx.invalid_start, idx.invalid_end + 1):
-                    line[j] = sum([line[idx.part1_of_mean], line[idx.part2_of_mean]]) / 2
-                line = average_fill(line, idx.part2_of_mean)
-                break
+    idx = FillingIndexes()
+    punch_in = False
+    for col, value in enumerate(line[recur_start_position::]):
+        if not punch_in and value == INVALID_DATA_VALUE:
+            idx.part1_of_mean = recur_start_position + col - 1
+            idx.invalid_start = recur_start_position + col
+            punch_in = True
+        if punch_in and value > INVALID_DATA_VALUE:
+            idx.invalid_end = recur_start_position + col - 1
+            idx.part2_of_mean = recur_start_position + col
+            for j in range(idx.invalid_start, idx.invalid_end + 1):
+                line[j] = sum([line[idx.part1_of_mean], line[idx.part2_of_mean]]) / 2
+            line = average_fill(line, idx.part2_of_mean)
+            break
 
     return line
 
 
-def linear_fill(line: list, start_position: int = 0) -> list:
+def linear_fill(line: list, recur_start_position: int) -> list:
     """
-    指定された行にある欠損値を線形補間を使用して埋めます
+    与えられた範囲を繰り返し、無効な値を線形に補間された値で置き換えます
     欠損値: INVALID_DATA_VALUE
 
-    :param line: 欠損値を埋める対象の行
-    :param start_position: 欠損値の補間を開始する位置（再帰的に変わる）
-    :return: 線形補間を用いて欠損値が補完された行
+    :param line:
+    :param recur_start_position:
+    :return: 線形に埋められた値を表すリスト
     """
-    if start_position is not None:
-        idx = FillingIndexes()
-        punch_in = False
-        for col, value in enumerate(line[start_position::]):
-            if not punch_in and value == INVALID_DATA_VALUE:
-                idx.part1_of_mean = start_position + col - 1
-                idx.invalid_start = start_position + col
-                punch_in = True
-            if punch_in and value > INVALID_DATA_VALUE:
-                idx.invalid_end = start_position + col - 1
-                idx.part2_of_mean = start_position + col
-                how_many_times = idx.part2_of_mean - idx.part1_of_mean + 1
-                tolerance = np.linspace(line[idx.part1_of_mean], line[idx.part2_of_mean], how_many_times)
-                for i, n_value in enumerate(tolerance):
-                    line[idx.part1_of_mean + i] = n_value
-                line = linear_fill(line, idx.part2_of_mean)
-                break
+    idx = FillingIndexes()
+    punch_in = False
+    for col, value in enumerate(line[recur_start_position::]):
+        if not punch_in and value == INVALID_DATA_VALUE:
+            idx.part1_of_mean = recur_start_position + col - 1
+            idx.invalid_start = recur_start_position + col
+            punch_in = True
+        if punch_in and value > INVALID_DATA_VALUE:
+            idx.invalid_end = recur_start_position + col - 1
+            idx.part2_of_mean = recur_start_position + col
+            how_many_times = idx.part2_of_mean - idx.part1_of_mean + 1
+            print(f'{line[0]}｜col: {col}, {idx}')
+            tolerance = np.linspace(line[idx.part1_of_mean], line[idx.part2_of_mean], how_many_times)
+            for i, n_value in enumerate(tolerance):
+                line[idx.part1_of_mean + i] = n_value
+            line = linear_fill(line, idx.part2_of_mean)
+            break
 
     return line
 
@@ -176,32 +187,33 @@ def manage_invalid_values_with_adjustment(r: RangeExtractor, output_records: lis
     #  硬盤有無: 硬盤があるか？の 0 1 列
     #  硬盤深度: 何センチ？の情報（50センチ未満で232が発生したら、が基準だがユーザー入力できるように）
     if r.punch_range.is_both_not_none():
-        start_pos = r.numeric_range.start_pos + r.punch_range.start_pos
-        output_records.append(linear_fill(list(r.raw), start_pos))
+        output_records.append(linear_fill(list(r.raw), r.numeric_range.start_pos))
     else:
         output_records.append(list(r.raw))
 
 
 def manage_invalid_values_without_adjustment(i, r: RangeExtractor, output_records: list):
     if r.punch_range.is_both_none():
-        error_message = f"{i + 1}行目のデータは無効なデータ値 {INVALID_DATA_VALUE} のみで構成されています。確認してください"
+        error_message = f"{i + 1}行目のデータは無効なデータ値 {INVALID_DATA_VALUE} のみで構成されています。確認してください({r.is_hard_pan()})"
         output_records.append([error_message])
         return
     # 両端の INVALID_DATA_VALUE を除外したスライスデータにします
     punch_range = r.raw.iloc[r.punch_range.start_pos:r.punch_range.end_pos + 1]
     for col, cell in enumerate(punch_range):
         if cell == INVALID_DATA_VALUE:
-            error_message = f"{i + 1}行目のデータには無効なデータ値 {INVALID_DATA_VALUE} が含まれています"
+            error_message = f"{i + 1}行目のデータには無効なデータ値 {INVALID_DATA_VALUE} が含まれています({r.is_hard_pan()})"
             output_records.append([error_message])
             break
 
 
-def find_invalid_records(data: pd.DataFrame, apply_adjustment: bool, output_directory: str = './output'):
+def find_invalid_records(data: pd.DataFrame, apply_adjustment: bool, hard_pan_cursor: HardPanCursor,
+                         output_directory: str = './output'):
     """
     指定されたデータ内で無効なレコードを見つけ、apply_adjustment が指定された場合には補正を適用します
 
     :param data: 検証するレコードを含むデータフレーム
     :param apply_adjustment: 無効なレコードに補正を適用するかどうか
+    :param hard_pan_cursor: 50センチ `未満` で232が発生したら の位置
     :param output_directory: 出力ファイルを保存するディレクトリ
     :return: 出力レコードのリスト
     """
@@ -210,7 +222,7 @@ def find_invalid_records(data: pd.DataFrame, apply_adjustment: bool, output_dire
 
     output_records = [] if not apply_adjustment else [list(data.columns)]
     for i, row in data.iterrows():
-        range_extractor = RangeExtractor(row)
+        range_extractor = RangeExtractor(row, hard_pan_cursor)
         if not apply_adjustment:
             manage_invalid_values_without_adjustment(i, range_extractor, output_records)
         else:
@@ -222,17 +234,18 @@ def find_invalid_records(data: pd.DataFrame, apply_adjustment: bool, output_dire
 if __name__ == "__main__":
     data_sample_path = 'input/キャロットFARM_soil_data-20240228.csv'
     save_path = 'output/processed_data.csv'
+    hard_pan_less_than_cm = HardPanCursor(50)
     df_csv = pd.read_csv(data_sample_path, encoding='shift-jis')
 
     print('エラー値の補正なし版（コンソールに出力するだけ）：')
-    errors = find_invalid_records(df_csv, apply_adjustment=False)
+    errors = find_invalid_records(df_csv, False, hard_pan_less_than_cm)
     for error in errors:
         print(error.pop())
 
     os.makedirs(name='output', exist_ok=True)
 
     print('\nエラー値の補正あり版：')
-    df = pd.DataFrame(find_invalid_records(df_csv, apply_adjustment=True))
+    df = pd.DataFrame(find_invalid_records(df_csv, True, hard_pan_less_than_cm))
     try:
         df.to_csv(save_path, encoding='shift-jis', header=False, index=False)
         print(f'{save_path} に出力が完了しました')
--- invalid_data/test_invalid_data.py
+++ invalid_data/test_invalid_data.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 import numpy as np
 import pandas as pd
 
-from invalid_data import average_fill, linear_fill, RangeExtractor
+from invalid_data import average_fill, linear_fill, RangeExtractor, HardPanCursor
 
 INVALID_DATA_VALUE = 232
 NUMERIC_RANGE_LABELS = ['圧力[kPa]1cm', '圧力[kPa]2cm', '圧力[kPa]3cm', '圧力[kPa]4cm', '圧力[kPa]5cm', '圧力[kPa]6cm',
@@ -13,57 +13,81 @@ FULL_RANGE_LABELS = ['取引先名', '住所', '圧力[kPa]1cm', '圧力[kPa]4cm
 
 
 class TestRecordValidator(TestCase):
+    def setUp(self):
+        self.hard_pan_less_than_cm = HardPanCursor(50)
+
     def test_get_cursor_of_spike_no_reverse_no_offset_with_invalid_value_on_both(self):
         data = [232, 350, 405, 301, 300, 232, 232, 232, 232, 232]
-        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS), self.hard_pan_less_than_cm)
         self.assertEqual(1, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE))
+        self.assertEqual(0, range_extractor.numeric_range.start_pos)
+        self.assertEqual(10, range_extractor.numeric_range.length)
+        self.assertEqual(1, range_extractor.punch_range.start_pos)
         self.assertEqual(4, range_extractor.punch_range.length)
         self.assertEqual(5, range_extractor.hard_pan_range.start_pos)
         self.assertEqual(5, range_extractor.hard_pan_range.length)
 
     def test_get_cursor_of_spike_no_reverse_no_offset_with_invalid_value_on_left(self):
         data = [232, 232, 232, 232, 232, 300, 301, 405, 350, 400]
-        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS), self.hard_pan_less_than_cm)
+        self.assertEqual(0, range_extractor.numeric_range.start_pos)
+        self.assertEqual(10, range_extractor.numeric_range.length)
+        self.assertEqual(5, range_extractor.punch_range.start_pos)
         self.assertEqual(5, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE))
         self.assertEqual(5, range_extractor.punch_range.length)
         self.assertIsNone(range_extractor.hard_pan_range)
 
     def test_get_cursor_of_spike_no_inverse_no_offset_with_invalid_value_on_right(self):
         data = [400, 350, 405, 301, 300, 232, 232, 232, 232, 232]
-        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS), self.hard_pan_less_than_cm)
         self.assertEqual(0, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE))
+        self.assertEqual(0, range_extractor.numeric_range.start_pos)
+        self.assertEqual(10, range_extractor.numeric_range.length)
+        self.assertEqual(0, range_extractor.punch_range.start_pos)
         self.assertEqual(5, range_extractor.punch_range.length)
         self.assertEqual(5, range_extractor.hard_pan_range.start_pos)
         self.assertEqual(5, range_extractor.hard_pan_range.length)
 
     def test_get_cursor_of_spike_reverse_no_offset(self):
         data = [232, 232, 232, 232, 300, 301, 405, 350, 232, 232]
-        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS), self.hard_pan_less_than_cm)
         self.assertEqual(7, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE, reverse=True))
+        self.assertEqual(0, range_extractor.numeric_range.start_pos)
+        self.assertEqual(10, range_extractor.numeric_range.length)
+        self.assertEqual(4, range_extractor.punch_range.start_pos)
         self.assertEqual(4, range_extractor.punch_range.length)
         self.assertEqual(8, range_extractor.hard_pan_range.start_pos)
         self.assertEqual(2, range_extractor.hard_pan_range.length)
 
     def test_get_cursor_of_spike_no_reverse_with_offset(self):
         data = ['あの会社', '北海道', 232, 232, 301, 405, 232, 232, 232, np.nan]
-        range_extractor = RangeExtractor(pd.Series(data, index=FULL_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=FULL_RANGE_LABELS), self.hard_pan_less_than_cm)
         self.assertEqual(2, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE))
+        self.assertEqual(2, range_extractor.numeric_range.start_pos)
+        self.assertEqual(7, range_extractor.numeric_range.length)
+        self.assertEqual(4, range_extractor.punch_range.start_pos)
         self.assertEqual(2, range_extractor.punch_range.length)
         self.assertEqual(6, range_extractor.hard_pan_range.start_pos)
         self.assertEqual(3, range_extractor.hard_pan_range.length)
 
     def test_get_cursor_of_spike_reverse_with_offset(self):
         data = ['あの会社', '北海道', 232, 232, 301, 405, 232, 232, 232, np.nan]
-        range_extractor = RangeExtractor(pd.Series(data, index=FULL_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=FULL_RANGE_LABELS), self.hard_pan_less_than_cm)
         self.assertEqual(3, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE, reverse=True))
+        self.assertEqual(2, range_extractor.numeric_range.start_pos)
+        self.assertEqual(7, range_extractor.numeric_range.length)
+        self.assertEqual(4, range_extractor.punch_range.start_pos)
         self.assertEqual(2, range_extractor.punch_range.length)
         self.assertEqual(6, range_extractor.hard_pan_range.start_pos)
         self.assertEqual(3, range_extractor.hard_pan_range.length)
 
     def test_get_cursor_of_spike_no_spike(self):
         data = [232, 232, 232, 232, 232, 232, 232, 232, 232, 232]
-        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS))
+        range_extractor = RangeExtractor(pd.Series(data, index=NUMERIC_RANGE_LABELS), self.hard_pan_less_than_cm)
         self.assertEqual(None, range_extractor.find_spike_point_in_line(INVALID_DATA_VALUE))
+        self.assertEqual(0, range_extractor.numeric_range.start_pos)
+        self.assertEqual(10, range_extractor.numeric_range.length)
+        self.assertEqual(None, range_extractor.punch_range.start_pos)
         self.assertEqual(None, range_extractor.punch_range.length)
         self.assertEqual(0, range_extractor.hard_pan_range.start_pos)
         self.assertEqual(10, range_extractor.hard_pan_range.length)
@@ -109,3 +133,20 @@ class TestLinearFill(TestCase):
         data = [1334, 232, 232, 1364, 232, 232, 1406]
         expected = [1334.0, 1344.0, 1354.0, 1364.0, 1378.0, 1392.0, 1406.0]
         self.assertEqual(expected, linear_fill(data))
+
+
+class TestHardPanCursor(TestCase):
+    def test_constructor(self):
+        self.assertEqual(HardPanCursor(30).value, 30)
+        self.assertEqual(HardPanCursor(1).value, 1)
+        self.assertEqual(HardPanCursor(60).value, 60)
+
+    def test_constructor_value_out_of_range(self):
+        with self.assertRaises(ValueError):
+            HardPanCursor(-1)
+            HardPanCursor(0)
+            HardPanCursor(61)
+
+    def test_str_method(self):
+        cursor = HardPanCursor(30)
+        self.assertEqual(str(cursor), "圧力[kPa]30cm")
